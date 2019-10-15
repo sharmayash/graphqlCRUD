781c6fae4c7f2b3c4eb87959d91b16d99781f4a7
yash <sharmayash44ys@gmail.com>
2019-10-15 23:34:06 +0530

    updated

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require("express");
 const graphqlHttp = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
+
+// cors middleware
+app.use(cors());
 
 mongoose.connect("mongodb://localhost/todocrud", {
   useNewUrlParser: true,
